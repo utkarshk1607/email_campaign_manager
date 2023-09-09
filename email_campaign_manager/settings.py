d88a43aeebb730d917e3b33a8f23f53b91ba05c7
utkarshk1607 <144452025+utkarshk1607@users.noreply.github.com>
2023-09-10 00:00:08 +0530

    Update settings.py

--- email_campaign_manager/settings.py
+++ email_campaign_manager/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zrgod^m42%x7+0i+^0$#3%*@5jl*qt-3_j=mcr!!dw&4yebgnc'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
